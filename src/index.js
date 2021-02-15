3370c2f2379f7bd4ea1ba7be6bd1f874da4f1a91
Alexander Korchagin <al_kor_00@bk.ru>
2021-02-16 00:39:47 +0300

    Start building the base

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log("Hello World!");
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const AppHeader = () => {
+  return (
+    <h1>My Goals List</h1>
+  )
+};
+
+const SearchPanel = () => {
+  const searchText = 'search...'
+  const searchStyle = {
+    fontSize: '20px'
+  };
+  
+  return <input
+  style={searchStyle}
+  placeholder={searchText}
+  />
+};
+
+const items = ['Do something', '???', 'Profit'];
+
+const GoalsList = () => {
+  return (
+    <ul>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
+      <li>{ items[2] }</li>
+    </ul>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <span>{ (new Date()).toString() }</span>
+      <AppHeader />
+      <SearchPanel />
+      <GoalsList />
+    </div>
+  );
+}
+
+ReactDom.render(<App />, document.getElementById('root'));

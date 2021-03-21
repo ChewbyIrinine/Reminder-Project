d53b18d5152b177771a49c028f0fe4bd6badcd78
Alexander Korchagin <al_kor_00@bk.ru>
2021-03-21 20:33:24 +0300

    Add state for GoalItem

--- src/componets/GoalItem/GoalItem.js
+++ src/componets/GoalItem/GoalItem.js
@@ -1,30 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './GoalItem.css'
 
 export default class GoalItem extends Component {
 
+    constructor() {
+        super();
+    }
+
+    state = {
+        done: false
+    }
+
+    onLabelClick = () => {
+        this.setState({
+            done: true
+        });
+    }
+
     render() {
 
         const { text, special = false } = this.props;
+        const { done } = this.state;
+
+        let classNames = 'goal-item';
+        if (done) {
+            classNames += ' done';
+        }
 
         const itemStyle = {
             color: special ? 'steelblue' : 'black',
             fontWeight: special ? 'bold' : 'normal'
         };
-    
+
         return (
-            <span className="goal-item">
+            <span className={classNames}>
                 <span className="goal-item-label"
-                    style={itemStyle}>
+                    style={itemStyle}
+                    onClick={this.onLabelClick}>
                     {text}
                 </span>
-    
+
                 <button type="button"
                     className="btn btn-outline-success btn-sm float-right">
                     <i className="fa fa-exclamation" />
                 </button>
-    
+
                 <button type="button"
                     className="btn btn-outline-danger btn-sm float-right">
                     <i className="fa fa-trash-o" />
